d95a5b83990a0637519beac1f11a2a4fbc7317d1
Jeroen Dekkers <jeroen@dekkers.ch>
2011-10-31 04:12:18 +0100

    Add the standard smoothness theme

--- django_jquery_ui/templatetags/jquery_ui_tags.py
+++ django_jquery_ui/templatetags/jquery_ui_tags.py
@@ -6,4 +6,6 @@ register = template.Library()
 
 @register.simple_tag
 def include_jquery_ui():
-    return "<script type='text/javascript' src='%s'></script>" % os.path.join(getattr(settings, 'STATIC_URL'), 'jquery-ui', 'jquery-ui-1.8.16.custom.min.js')
+    return """<script type='text/javascript' src='%s'></script>
+<link type='text/css' href='%s' rel='Stylesheet' />""" % (os.path.join(getattr(settings, 'STATIC_URL'), 'jquery-ui', 'jquery-ui-1.8.16.custom.min.js'),
+                                                          os.path.join(getattr(settings, 'STATIC_URL'), 'jquery-ui', 'jquery-ui-1.8.16.custom.css'))
