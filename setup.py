3b8f8debfe2de339a51b82dae371e2f271c3626c
Jeroen Dekkers <jeroen@dekkers.ch>
2011-10-31 04:12:49 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-jquery-ui',
-    version = '0.1.1',
+    version = '0.2',
     description = "jquery-ui django app - bundles jQuery UI for django-staticfiles",
     long_description = open('README.rst').read(),
     author = 'Aljosa Mohorovic',
